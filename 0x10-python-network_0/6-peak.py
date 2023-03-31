98f05a219db12f031e1ef7c1efaae96c45adee26
Bobbs04 <win3ldestiny@gmai.com>
2023-03-31 06:05:55 -0700

    Python net

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-""" Finds a peak inside a list """
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
+    """ Finds the peak in a list of integers """
     if list_of_integers == []:
         return None
 
     length = len(list_of_integers)
-    mid = int(length / 2)
+    m = int(length / 2)
     li = list_of_integers
 
-    if mid - 1 < 0 and mid + 1 >= length:
-        return li[mid]
-    elif mid - 1 < 0:
-        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
-    elif mid + 1 >= length:
-        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
 
-    if li[mid - 1] < li[mid] > li[mid + 1]:
-        return li[mid]
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
 
-    if li[mid + 1] > li[mid - 1]:
-        return find_peak(li[mid:])
-    return find_peak(li[:mid])
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
